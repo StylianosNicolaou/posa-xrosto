737584c4c1017d537fb8b822e89c899be35694fe
StylianosNicolaou <nicolaou.s24@live.unic.ac.cy>
2025-07-14 00:49:55 +0300

    change in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type React from "react"
-import type { Metadata, Viewport } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Posa Xrosto - Bill Splitter",
-  description: "Easily calculate who owes what when sharing plates at a restaurant",
+  description:
+    "Easily calculate who owes what when sharing plates at a restaurant",
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
@@ -21,28 +22,30 @@ export const metadata: Metadata = {
     type: "website",
     siteName: "Posa Xrosto",
     title: "Posa Xrosto - Bill Splitter",
-    description: "Easily calculate who owes what when sharing plates at a restaurant",
+    description:
+      "Easily calculate who owes what when sharing plates at a restaurant",
   },
   twitter: {
     card: "summary",
     title: "Posa Xrosto - Bill Splitter",
-    description: "Easily calculate who owes what when sharing plates at a restaurant",
+    description:
+      "Easily calculate who owes what when sharing plates at a restaurant",
   },
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export const viewport: Viewport = {
   themeColor: "#6a7fdb",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-}
+  // maximumScale: 1,
+  // userScalable: false,
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -57,7 +60,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               if ('serviceWorker' in navigator) {
@@ -73,8 +76,8 @@ export default function RootLayout({
               }
             `,
           }}
-        />
+        /> */}
       </body>
     </html>
-  )
+  );
 }
